1704b2d97a830075bfc5a33ad0abc7a618baf88e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-07 03:18:18 +0530

    Made home page

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING TO PORT 3000!!");
+});
