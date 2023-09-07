29fcf4bc36b96c33c2c14f1deeae20269554e55b
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-08 05:09:25 +0530

    Boilerplate

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const ejsMate = require("ejs-mate");
 const app = express();
 const path = require("path");
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
