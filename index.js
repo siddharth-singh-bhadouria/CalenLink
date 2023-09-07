9c0449810dcbe572528b8c513d8f1ae709119035
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-08 04:48:20 +0530

    Login page

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.listen(3000, () => {
   console.log("LISTENING TO PORT 3000!!");
 });
