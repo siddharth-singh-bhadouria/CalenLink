200f1c8a697ea7b0ed74f06ec4b61e4e57425018
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-08 05:45:07 +0530

    Added rest of the pages

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("pages/home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+
+app.get("/services", (req, res) => {
+  res.render("pages/services");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("pages/contact");
 });
 
 app.get("/login", (req, res) => {
