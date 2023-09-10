01f9e823f092c425ebb0e74bd679a4ebab6a9e42
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-10 23:52:08 +0530

    Adding session 2

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
