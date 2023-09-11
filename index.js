e6caee82812ff62ec20c8ac439e462f8221da736
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-11 10:45:11 +0530

    Using passport

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -5,6 +9,14 @@ const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "your-mysql-host",
+  user: "your-username",
+  password: "your-password",
+  database: "your-database-name",
+});
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -12,6 +24,29 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+const secret = process.env.SECRET || "thisshouldbeabettersecret";
+
+app.use(
+  session({
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("pages/home");
