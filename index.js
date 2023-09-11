c0804784ee07ee5be08313ca8fa0c7d8df1f1160
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-12 03:19:47 +0530

    Configuring passport

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const crypto = require("crypto");
 const mysql = require("mysql2");
 
 const userRoutes = require("./routes/users");
@@ -45,6 +46,44 @@ app.use(
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
+  })
+);
+
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    db.get(
+      "SELECT * FROM users WHERE username = ?",
+      [username],
+      function (err, row) {
+        if (err) {
+          return cb(err);
+        }
+        if (!row) {
+          return cb(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+
+        crypto.pbkdf2(
+          password,
+          row.salt,
+          310000,
+          32,
+          "sha256",
+          function (err, hashedPassword) {
+            if (err) {
+              return cb(err);
+            }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+              return cb(null, false, {
+                message: "Incorrect username or password.",
+              });
+            }
+            return cb(null, row);
+          }
+        );
+      }
+    );
   })
 );
 
---
+++ models/users.js
@@ -0,0 +1,12 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(
+  `mysql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+);
+
+const User = sequelize.define("User", {
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
