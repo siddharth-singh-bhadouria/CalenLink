c90242c552aa273f9a3f5ec7bbae617af271c447
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-18 23:55:11 +0530

    Reconnecting mysql 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
@@ -14,11 +16,67 @@ db.connect((err) => {
   console.log("Connected to MySQL.");
 });
 
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000;
+app.get("/", (req, res) => {
+  res.render("pages/home");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// CREATE
+
+app.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+app.post("/register", (req, res) => {
+  let data = {
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+  };
+  let sql = "INSERT INTO user SET ?";
+  db.query(sql, data, (err, results) => {
+    if (err) throw err;
+    res.send("User added.");
+  });
+});
+
+// // READ
+// app.get("/users", (req, res) => {
+//   let sql = "SELECT * FROM users";
+//   db.query(sql, (err, results) => {
+//     if (err) throw err;
+//     res.send(results);
+//   });
+// });
+
+// // UPDATE
+// app.put("/update/:id", (req, res) => {
+//   let sql = `UPDATE users SET name = '${req.body.name}', age = '${req.body.age}' WHERE id = ${req.params.id}`;
+//   db.query(sql, (err, results) => {
+//     if (err) throw err;
+//     res.send("User updated.");
+//   });
+// });
+
+// // DELETE
+// app.delete("/delete/:id", (req, res) => {
+//   let sql = `DELETE FROM users WHERE id = ${req.params.id}`;
+//   db.query(sql, (err, results) => {
+//     if (err) throw err;
+//     res.send("User deleted.");
+//   });
+// });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
